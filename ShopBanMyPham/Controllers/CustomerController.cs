3d59872050605778c837deda37d20b736c70308a
Anh Tien Dep Trai <tiensd17052001@example.com>
2023-04-25 22:52:11 +0700

    done

---
+++ ShopBanMyPham/Controllers/CustomerController.cs
@@ -0,0 +1,63 @@
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using ShopBanMyPham.Models;
+using System.Drawing;
+using System.Text.RegularExpressions;
+
+namespace ShopBanMyPham.Controllers
+{
+	public class CustomerController : Controller
+	{
+		ShopBanMyPhamContext _context = null;
+
+		public INotyfService _notyfService { get; }
+
+		public CustomerController(INotyfService notyfService)
+		{
+			_context= new ShopBanMyPhamContext();
+			_notyfService=notyfService;
+		}
+		public IActionResult UpdateInfo(string fullname, string address, string email, string phoneNumber)
+		{
+            string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+			if (!Regex.IsMatch(email, pattern))
+			{
+				_notyfService.Success("Email không đúng định dạng");
+				return RedirectToAction("Payment", "Cart");
+			}
+			string pattern2 = @"^\d+$";
+			if (!Regex.IsMatch(phoneNumber, pattern2))
+			{
+                _notyfService.Success("PhoneNumber phải là số");
+                return RedirectToAction("Payment", "Cart");
+            }
+
+
+            var userName = HttpContext.Session.GetString("UserName");
+            var user = _context.Users.FirstOrDefault(x => x.Username == userName);
+			var customer = _context.Customers.FirstOrDefault(x => x.UserId == user.UserId);
+			if(customer == null)
+			{
+				Customer c = new Customer();
+				c.UserId = user.UserId;
+				c.CustomerName = fullname;
+				c.Address = address;
+				c.Email= email;
+				c.PhoneNumber = phoneNumber;
+				_context.Customers.Add(c);
+				_context.SaveChanges();
+			}
+			else
+			{
+                customer.UserId = user.UserId;
+                customer.CustomerName = fullname;
+                customer.Address = address;
+                customer.Email = email;
+                customer.PhoneNumber = phoneNumber;
+				_context.SaveChanges();
+            }
+            _notyfService.Success("Cập nhật thông tin khách hàng thành công");
+            return RedirectToAction("Payment", "Cart");
+		}
+	}
+}
---
+++ ShopBanMyPham/Controllers/ProductController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ShopBanMyPham.Models;
+using X.PagedList;
+
+namespace ShopBanMyPham.Controllers
+{
+    public class ProductController : Controller
+    {
+        ShopBanMyPhamContext _context = null;
+
+        private readonly Microsoft.AspNetCore.Hosting.IHostingEnvironment hostingEnvironment;
+
+        public ProductController(Microsoft.AspNetCore.Hosting.IHostingEnvironment environment)
+        {
+            _context = new ShopBanMyPhamContext();
+            hostingEnvironment = environment;
+        }
+        public IActionResult ProductByCategory(int? page, int id)
+        {
+            int pageSize = 10;
+            int pageNumber = page == null || page < 0 ? 1 : page.Value;
+            var products = _context.Products.Where(x => x.CategoryId == id).ToList();
+            PagedList<Product> res = new PagedList<Product>(products, pageNumber, pageSize);
+            ViewBag.Category = _context.Categories.ToList();
+            ViewBag.CategorySearch = id;
+            return View(res);
+        }
+
+        public IActionResult ProductDetail(int id) 
+        {
+            var product = _context.Products.FirstOrDefault(x => x.ProductId== id);
+            ViewBag.ListFavAlso = _context.Products.Where(x => x.CategoryId == product.CategoryId && x.ProductId != product.ProductId).ToList();
+            return View(product);
+        }
+
+        public IActionResult Search(int? page, string searchString)
+        {
+            int pageSize = 10;
+            int pageNumber = page == null || page < 0 ? 1 : page.Value;
+            var products = _context.Products.Where(x => x.ProductName.Contains(searchString)).ToList();
+            PagedList<Product> res = new PagedList<Product>(products, pageNumber, pageSize);
+            ViewBag.SearchString = searchString;
+            ViewBag.Category = _context.Categories.ToList();
+            return View(res);
+        }
+
+        public async Task<IActionResult> AddProduct()
+        {
+            ViewBag.CategoryId = new SelectList(_context.Categories.ToList(), "CategoryId", "CategoryName");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(Product product, IFormFile image)
+        {
+            if (ModelState.IsValid)
+            {
+                if (image != null && image.Length > 0)
+                {
+                    var imagePath = Path.Combine(hostingEnvironment.WebRootPath, "assets/product", image.FileName);
+                    using (var stream = new FileStream(imagePath, FileMode.Create))
+                    {
+                        await image.CopyToAsync(stream);
+                    }
+                    product.Image = image.FileName;
+                }
+
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+            ViewBag.CategoryId = new SelectList(_context.Categories.ToList(), "CategoryId", "CategoryName");
+            return View();
+        }
+    }
+}
